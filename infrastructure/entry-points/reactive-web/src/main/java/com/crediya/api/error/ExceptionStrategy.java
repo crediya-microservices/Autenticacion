e0b0743286fef1f88aa4cea52f0a26ac66778bcc
Andres Felipe Gañan Moreno <aganan@ADADOM.LOCAL>
2025-08-24 21:37:14 -0500

    ADD: se agrega libreria con las fuciones generales

--- infrastructure/entry-points/reactive-web/src/main/java/com/crediya/api/error/ExceptionStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.crediya.api.error;
-
-import org.springframework.http.HttpStatus;
-
-public interface ExceptionStrategy {
-    boolean supports(Throwable ex);
-    HttpStatus getStatus(Throwable ex);
-    String getError(Throwable ex);
-}
